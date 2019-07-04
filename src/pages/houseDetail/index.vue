92a211b7c19d9d7afc12197eb808b8b589f48288
littleluckly <707798472@qq.com>
2019-07-04 22:48:12 +0800

    详情页使用原生组件swiper实现轮播图

--- src/pages/houseDetail/index.vue
+++ src/pages/houseDetail/index.vue
@@ -1,12 +1,31 @@
 <template>
-    <div>详情</div>
+    <div>
+        <swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+            <block v-for="(item, index) in imgList" :key="index">
+                <swiper-item>
+                    <image :src="item.src" class="slide-image" mode="aspectFill" />
+                </swiper-item>
+            </block>
+        </swiper>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            value1: ''
+            indicatorDots: true,
+            autoplay: true,
+            interval: 3000,
+            duration: 500,
+            value1: '',
+            imgList: [
+                { src: 'https://pic5.58cdn.com.cn/anjuke_58/cc29706ea6857b65dab56f9f67add4c6?w=640&h=480&crop=1' },
+                { src: 'https://pic6.58cdn.com.cn/anjuke_58/599bef62e817176cfe7fd42014da45ca?w=640&h=480&crop=1' },
+                { src: 'https://pic5.58cdn.com.cn/anjuke_58/22eec5a3cf94ff9593e17b9c5124ba84?w=640&h=480&crop=1' },
+                { src: 'https://pic6.58cdn.com.cn/anjuke_58/0dc0ee4030643470a6247063823ab639?w=640&h=480&crop=1' },
+                { src: 'https://pic4.58cdn.com.cn/anjuke_58/749a8fafd17d5a7791bcda127ee8a4f2?w=640&h=480&crop=1' }
+            ]
         }
     },
     computed: {},
@@ -15,5 +34,10 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+.imgMap {
+    li {
+        float: left;
+    }
+}
 </style>
