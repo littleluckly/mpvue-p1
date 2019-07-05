d23e8c92ba668c5dd894e1ace254e5c9b5f2de1e
littleluckly <707798472@qq.com>
2019-07-05 13:11:47 +0800

    详情页开发

--- src/pages/houseDetail/index.vue
+++ src/pages/houseDetail/index.vue
@@ -1,12 +1,57 @@
 <template>
     <div>
-        <swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
-            <block v-for="(item, index) in imgList" :key="index">
-                <swiper-item>
-                    <image :src="item.src" class="slide-image" mode="aspectFill" />
-                </swiper-item>
-            </block>
-        </swiper>
+        <div class="pictureArea">
+            <swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+                <block v-for="(item, index) in imgList" :key="index">
+                    <swiper-item>
+                        <image :src="item.src" class="slide-image" mode="aspectFill" />
+                    </swiper-item>
+                </block>
+            </swiper>
+            <div>
+                <p class="title">6号线坂雪岗站，精装修，拎包入住，舒适干净整洁，随时入住</p>
+                <p class="statics">
+                    <span>更新：2019-07-05</span>
+                    <span>浏览：1次</span>
+                </p>
+            </div>
+        </div>
+        <div class="baseInfo">
+            <div class="importantInfo">
+                <div class="item">
+                    <p class="label">户型</p>
+                    <p class="value">1室1厅1卫</p>
+                </div>
+                <div class="item">
+                    <p class="label">租金</p>
+                    <p class="value">1200元/月</p>
+                </div>
+                <div class="item">
+                    <p class="label">类型</p>
+                    <p class="value">整租</p>
+                </div>
+            </div>
+            <div class="info">
+                <div class="infoItem">
+                    <p class="label">小区：</p>
+                    <p class="value">天安云谷城市广场天安云谷城市广场天安云谷城市广场</p>
+                </div>
+                <div class="infoItem">
+                    <p class="label">面积：</p>
+                    <p class="value">22平方</p>
+                </div>
+                <div class="clear"></div>
+                <div class="infoItem">
+                    <p class="label">楼层：</p>
+                    <p class="value">2层/总30层</p>
+                </div>
+                <div class="infoItem">
+                    <p class="label">付款：</p>
+                    <p class="value">押一付一</p>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -35,9 +80,69 @@ export default {
 </script>
 
 <style lang="less">
-.imgMap {
-    li {
-        float: left;
+page {
+    background: #f2f2f2;
+    font-size: 14px;
+}
+.clear {
+    clear: both;
+}
+.pictureArea {
+    margin-bottom: 10px;
+    background: #ffffff;
+    .title {
+        padding: 10px;
+        font-size: 20px;
+    }
+    .statics {
+        display: flex;
+        justify-content: space-between;
+        padding: 10px;
+    }
+}
+.baseInfo {
+    background: #ffffff;
+    padding: 0 10px;
+    .importantInfo {
+        display: flex;
+        justify-content: center;
+        padding: 20px 0;
+        border-bottom: 1px solid #f1f1f1;
+        .item {
+            flex-grow: 1;
+            text-align: center;
+            .label {
+                margin-bottom: 5px;
+                font-size: 12px;
+                color: #999;
+            }
+            .value {
+                color: #eb5f00;
+                font-size: 16px;
+            }
+        }
+    }
+    .info {
+        padding: 20px 0;
+        // display: flex;
+
+        .infoItem {
+            display: flex;
+            float: left;
+            width: 50%;
+            vertical-align: top;
+            padding-right: 10px;
+            box-sizing: border-box;
+
+            .label {
+                color: #666;
+                width: 50px;
+            }
+            .value {
+                flex: 1;
+                color: #333;
+            }
+        }
     }
 }
 </style>
